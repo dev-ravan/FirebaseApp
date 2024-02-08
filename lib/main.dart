68f52d2213432db398049e494788b0c40c8d98e2
dev-ravan <velmurugan1211r@gmail.com>
2024-02-08 09:30:30 +0530

    feat: firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:firebase_app/Utils/exports.dart';
 import 'package:firebase_app/Utils/splash.dart';
+import 'package:firebase_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
